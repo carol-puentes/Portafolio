f74a4c4842a8266ef8b5ef8fd69a5c381fad7a1f
carol-puentes <122763536+carol-puentes@users.noreply.github.com>
2025-03-25 19:42:53 -0500

    Mejoras de SEO y sitemap

---
+++ src/routes.js
@@ -0,0 +1,9 @@
+const routes = [
+    "/",          // Página principal
+    "/about",     // Si existe una página "Acerca de"
+    "/tecnologis", // Tecnologías usadas
+    "/proyectos"  // Página de proyectos
+  ];
+  
+  export default routes;
+  
