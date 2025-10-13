bee40b5166f5c368592655556ab022a9e5d97a5b
carol-puentes <122763536+carol-puentes@users.noreply.github.com>
2025-10-13 13:58:53 -0500

    Nuevos proyectos

--- src/data/data.js
+++ src/data/data.js
@@ -4,6 +4,8 @@ import triqui from '../assets/triqui.png'
 import wonders from '../assets/Wonders.png'
 import independiente from '../assets/independiente.png'
 import formulario from '../assets/formulario.png'
+import tomato from '../assets/tomato.png'
+import libro from '../assets/libro.png'
 
 export const dataPortfolio = [
  
@@ -55,6 +57,22 @@ export const dataPortfolio = [
       url: "https://formulario-registro-interactivo.netlify.app",
       repo:"",
     },
-  
+    { 
+      id: 7,
+      title: "projects.project7.title", // Clave de traducción
+      description: "projects.project7.description",
+      image: tomato,
+      url: "https://www.figma.com/proto/jDLu3xG241VD0BnsQM3HZQ/Untitled?node-id=1-321&p=f&t=iNiYRI4LZySgryjO-1&scaling=min-zoom&content-scaling=fixed&page-id=0%3A1",
+      repo:"",
+    },
+    {
+      id:8,
+      title: "projects.project8.title", // Clave de traducción
+      description: "projects.project8.description",
+      image: libro,
+      url: " https://www.figma.com/proto/NIovTzRcKH4U8aKZVK4Oz0/Untitled?node-id=1-3&starting-point-node-id=1%3A3&t=esLz6oclFifZz4tj-1",
+      repo:"",
+    }
+   
   ];
   
